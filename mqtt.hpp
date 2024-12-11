2a53b944f887fa8fe615777b00924862a2afaa8e
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-12-11 23:36:11 +0100

    updating headers

--- mqtt.hpp
+++ mqtt.hpp
@@ -1,26 +1,47 @@
 // ###############################
-// #    SMARTSHOP MQTT-TOPICS    #
+// #    circlelab MQTT-TOPICS    #
 // ###############################
+
+# pragma once
+
+// Libraries
+# include <WiFi.h>
+# include <PubSubClient.h>
+
+# include "network.hpp"
 
 // Functions
 # define JOIN_TOPICS(__t1, __t2) __t1 "/" __t2
 
 // Topics
-# define TOPIC_MODULES                  "smartshop/modules"
+# define TOPIC_MODULES                  "circlelab/modules"
 
-# define TOPIC_TEMP                     "smartshop/temp"
-# define TOPIC_HUMID                    "smartshop/humid"
+# define TOPIC_TEMP                     "circlelab/temp"
+# define TOPIC_HUMID                    "circlelab/humid"
 
-# define TOPIC_LIGHT_CHAIN_MAIN         "smartshop/light/chain_main"
-# define TOPIC_LIGHT_CHAIN_LIVING       "smartshop/light/chain_living"
+# define TOPIC_LIGHT_CHAIN_MAIN         "circlelab/light/chain_main"
+# define TOPIC_LIGHT_CHAIN_LIVING       "circlelab/light/chain_living"
 
 // MQTT data structure
-namespace smartshop {
-    static float humid = 0.0;
-    static float temp = 0.0;
+namespace circlelab {
+  static float humid = 0.0;
+  static float temp = 0.0;
 
-    namespace light {
-        static bool chain_main = false;
-        static bool chain_living = false;
+  namespace light {
+    static bool chain_main = false;
+    static bool chain_living = false;
+  }
+}
+
+namespace cosy {
+  namespace remote {
+    typedef void (*MQTTCallback)(char*, uint8_t*, unsigned int);
+
+    void connect_mqtt(PubSubClient* mqtt_client, MQTTCallback mqtt_callback) {
+      IPAddress hub_ip = resolve_hub();
+       
+      mqtt_client->setServer(hub_ip, COSY_HUB_MQTT_PORT);
+      mqtt_client->setCallback(mqtt_callback);
     }
+  }
 }
--- network.hpp
+++ network.hpp
@@ -1,7 +1,59 @@
-// ##################################
-// #    SMARTSHOP NETWORK-TOPICS    #
-// ##################################
+// ##########################
+// #    SMARTSHOP NETWORK   #
+// ##########################
 
-# define SMARTSHOP_HUB_HOSTNAME "hub.local"
+# pragma once
 
-# define SMARTSHOP_HUB_MQTT_PORT 1883
+# include <WiFi.h>
+
+# include <sylo/logging.hpp>
+
+# include "private/wifi_creds.hpp"
+
+/// @brief Hostname of the Hub
+# define COSY_HUB_HOSTNAME "hub.local"
+
+// Ports
+# define COSY_HUB_MQTT_PORT 1883
+
+namespace cosy {
+  namespace remote {
+    void connect(const char* hostname) {
+      // Configure WiFi
+      WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+      WiFi.setHostname(hostname);
+      WiFi.mode(WIFI_MODE_STA);
+      WiFi.begin(PRIVATE_WIFI_SSID, PRIVATE_WIFI_PWD);
+
+      // Connection attempt
+      log_info("| > Connecting ");
+      while (WiFi.status() != WL_CONNECTED) {
+        log_info(".");
+        delay(500);
+      }
+      log_infoln(" done!");
+      
+      log_info("| | > IP: ");
+      log_infoln(WiFi.localIP());
+
+      log_info("| | > RRSI: ");
+      log_infoln(WiFi.RSSI());
+    }
+
+    /// @brief Resolves the IP-Address of the hub-server when connected to a WiFi
+    /// @return The IP-Address of the hub
+    IPAddress resolve_hub() {
+      IPAddress hub_ip;
+
+      log_info("> Resolving hub hostname ... ");
+      WiFi.hostByName(COSY_HUB_HOSTNAME, hub_ip);
+      log_infoln("done!");
+
+      log_debug("| > IP: '");
+      log_debug(hub_ip);
+      log_debugln("'");
+
+      return hub_ip;
+    }
+  }
+}
